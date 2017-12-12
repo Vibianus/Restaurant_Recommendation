a6b43465ac61b54ccc10a5f2b956dc17ec0da3ac
Chester <j6fm06503@gmail.com>
2017-12-12 17:07:54 +0800

    test

--- app.py
+++ app.py
@@ -172,8 +172,8 @@ def check_stat_and_recommend(message_text, stat_result, recipient_id):
         global_stat['time'] = stat_result['result']['time']
         global_stat['location'] = stat_result['result']['location']
         for key in stat_result['result'] :
-            log(key)
-            log(stat_result['result']['key'])
+            log(str(key))
+            log(str(stat_result['result'][key]))
         stat_result['result']['intent'] = 'N'
         stat_result['result']['time'] = ''
         stat_result['result']['location'] = ''
